ee7cb9a0330d3d2f451e26dedaa982bb3cb7aa02
Eason Chen <easonchen@EasontekiiMac.local>
2014-06-21 22:27:03 +0800

    test

---
+++ GitTest/GitTest/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  GitTest
+//
+//  Created by Eason Chen on 14/6/21.
+//  Copyright (c) 2014年 Eason Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
